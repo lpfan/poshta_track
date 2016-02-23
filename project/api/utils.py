ac98b058454db8ea62e77b23bc03faabd54ba02f
Rudy <misha.shchetinin@gmail.com>
2016-02-23 22:58:10 +0200

    added soap method

--- project/api/utils.py
+++ project/api/utils.py
@@ -1,5 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+from rinse.message import SoapMessage
+from rinse.client import SoapClient
+from lxml import etree
+from rinse.util import printxml
+
+
+def get_parsel_via_soap(parsel_code):
+    url = 'http://services.ukrposhta.com/barcodestatistic/barcodestatistic.asmx?WSDL'
+    body = etree.Element('GetBarcodeInfo')
+    guid = etree.SubElement(body, 'guid')
+    guid.text = 'fcc8d9e1-b6f9-438f-9ac8-b67ab44391dd'
+    culture = etree.SubElement(body, 'culture')
+    culture.text = 'uk'
+    barcode = etree.SubElement(body, 'barcode')
+    barcode.text = parsel_code
+    msg = SoapMessage(body)
+    client = SoapClient(url, debug=True)
+    resp = client(msg)
 
 
 def get_parsel_status_html(parsel_code):
