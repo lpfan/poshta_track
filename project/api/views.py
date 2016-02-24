246e96d697f9a482f173861daced77f7f75e928f
Misha Shchetinin <mshchetinin@logika.com>
2016-02-24 20:31:02 +0200

    added featured viewset

--- project/api/views.py
+++ project/api/views.py
@@ -1,11 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api import utils
+from api import serializers, utils
 
 
 class ParselStatus(APIView):
 
     def get(self, request, parsel_code):
         parsel_status = utils.get_parsel_status(parsel_code)
-        return Response({'status': parsel_status})
+        serializer = serializers.ParselSerializer(parsel_status)
+        return Response(serializer.data)
+
+
+class ParselViewSet():
+    pass
