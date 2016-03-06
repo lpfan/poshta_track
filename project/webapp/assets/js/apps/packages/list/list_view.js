0c96f0a7a1ea6c555810d863d0c8603f3bc064c2
Rudy <misha.shchetinin@gmail.com>
2016-03-07 00:02:02 +0200

    stock in the delete functionality

--- project/webapp/assets/js/apps/packages/list/list_view.js
+++ project/webapp/assets/js/apps/packages/list/list_view.js
@@ -1,12 +1,34 @@
 Tracker.module("PackagesApp.List", function (List, Tracker, Backbone, Marionette, $, _) {
   List.Package = Marionette.ItemView.extend({
     tagName: "tr",
-    template: "#package-list-item"
+    template: "#package-list-item",
+
+    events: {
+      "click": "highlightBarcode",
+      "click .js-delete-package": "deletePackage"
+    },
+
+    deletePackage: function(e) {
+      e.stopPropagation();
+      this.model.collection.remove(this.model);
+    },
+
+    highlightBarcode: function (e) {
+      e.preventDefault();
+      this.$el.toggleClass("warning");
+    },
+
+    remove: function () {
+      this.$el.fadeOut();
+    }
+
   });
 
-  List.Packages = Marionette.CollectionView.extend({
+  List.Packages = Marionette.CompositeView.extend({
     tagName: "table",
     className: "table table-hover",
-    childView: List.Package
+    childView: List.Package,
+    template: "#package-list",
+    itemViewContainer: "tbody"
   });
 });
